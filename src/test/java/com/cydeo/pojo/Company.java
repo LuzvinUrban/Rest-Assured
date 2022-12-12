c8000311a3f7453cac1404190614f6b5a75d5af1
Luzvin Urban <lucybdk@gmail.com>
2022-12-12 18:24:39 -0500

    API4

---
+++ src/test/java/com/cydeo/pojo/Company.java
@@ -0,0 +1,29 @@
+package com.cydeo.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class Company{
+
+        private String companyName;
+        private Address address;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
