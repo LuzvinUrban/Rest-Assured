d3b37f630f58e6177142024ff9d4e806cf70168d
Luzvin Urban <lucybdk@gmail.com>
2022-12-12 10:35:29 -0500

    API2

---
+++ src/test/java/com/cydeo/pojo/Link.java
@@ -0,0 +1,16 @@
+package com.cydeo.pojo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+    @Data
+
+    public class Link {
+    private String rel;
+    private String href;
+
+
+}
